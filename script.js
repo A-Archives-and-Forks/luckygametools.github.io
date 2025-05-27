f2c486fba63c553ed39edf81e3870621dfb1a188
luckygametools <luckygametools@gmail.com>
2025-05-27 09:56:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // JavaScript file for Lucky Game Tools
 // Currently no interactive functionality is needed
 // This file is included for future enhancements
+var downloadUrl1="https://gofile.io/d/7x1sZ9";
+var downloadUrl2="https://ranoz.gg/file/cPOv0ao6";
 
 document.addEventListener('DOMContentLoaded', function() {
     // Page loaded successfully
     console.log('LuckyGameTools page loaded');
-    
+    document.getElementById('download1').href=downloadUrl1;
+    document.getElementById('download2').href=downloadUrl2;
     // Future JavaScript functionality can be added here
     // For example: form validation, dynamic content loading, animations, etc.
 });
